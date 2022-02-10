00a181985b2e8cd06e75f9408de76d8b0cbf2900
David <2305432+davortsan@users.noreply.github.com>
2022-02-10 22:33:26 +0100

    Add files via upload

---
+++ TeamsStorageUsageCurrent.ps1
@@ -0,0 +1,33 @@
+﻿Import-Module MicrosoftTeams
+Import-Module ExchangeOnlineManagement
+Import-Module Microsoft.Online.SharePoint.PowerShell
+
+
+
+$AdminSiteURL = "https://<tenant_name>-admin.sharepoint.com"
+$result = New-Object -TypeName 'System.Collections.ArrayList';
+[System.Collections.ArrayList]$result= @()
+
+$cred = Get-Credential
+
+Connect-MicrosoftTeams -Credential $cred
+Connect-ExchangeOnline -Credential $cred
+Connect-SPOService -Url $AdminSiteURL -Credential $cred
+
+$Teams = Get-Team | select GroupId
+
+foreach ($actual in $Teams)
+{
+    $SPOSite = Get-Unifiedgroup -Identity $actual.GroupId | Select SharePointSiteurl
+
+    if ($SPOSite.SharePointSiteUrl -ne $null)
+    { 
+        $data = Get-SPOSite $SPOSite.SharePointSiteUrl | select Title, StorageUsageCurrent
+
+        $data.Title+';'+$data.StorageUsageCurrent
+    }
+}
+
+Disconnect-MicrosoftTeams
+Disconnect-ExchangeOnline
+Disconnect-SPOService
